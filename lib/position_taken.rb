2b812a43d8b0bc6dd4604ae89a1051a17ee24ad2
Nazar Turkmen <nazar.rudn@gmail.com>
2020-04-26 03:10:17 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,11 @@
  def position_taken?(board=[" "," "," "," "," "," "," "," "," "], index)
    if board[index]==" "
      return false
+   end
  end
 
 
 
 # code your #position_taken? method here!
+chitecture-9667/ttt-6-position-t
+aken-rb-cb-gh-000/lib/position_taken.rb:8: syntax error, unexpected end-of-input, expecting end (SyntaxError)
