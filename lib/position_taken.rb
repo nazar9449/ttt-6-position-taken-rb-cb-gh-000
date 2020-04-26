ad7d77e448930c07b1f41eaf3d87474a88baa8a5
Nazar Turkmen <nazar.rudn@gmail.com>
2020-04-26 03:13:48 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
+ def position_taken?(board=[" "," "," "," "," "," "," "," "," "], index)
+   if board[index] == " " || board[index] == "" || board[index] == nil
+     return false
+   elsif board[index] == "X" || board[index] == "O"
+     return true
+   end
+ end
+
+
+
 # code your #position_taken? method here!
+#chitecture-9667/ttt-6-position-t
+#aken-rb-cb-gh-000/lib/position_taken.rb:8: syntax error, unexpected end-of-input, expecting end (SyntaxError)
