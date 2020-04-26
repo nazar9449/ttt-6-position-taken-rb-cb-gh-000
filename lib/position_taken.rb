38152e1923dbd7df8b3c2de82ef34f7d17730be0
Nazar Turkmen <nazar.rudn@gmail.com>
2020-04-26 03:11:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -7,5 +7,5 @@
 
 
 # code your #position_taken? method here!
-chitecture-9667/ttt-6-position-t
-aken-rb-cb-gh-000/lib/position_taken.rb:8: syntax error, unexpected end-of-input, expecting end (SyntaxError)
+#chitecture-9667/ttt-6-position-t
+#aken-rb-cb-gh-000/lib/position_taken.rb:8: syntax error, unexpected end-of-input, expecting end (SyntaxError)
