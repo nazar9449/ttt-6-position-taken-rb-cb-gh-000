ef0b146f0ca06d304f123915177462b8bb938cf2
Nazar Turkmen <nazar.rudn@gmail.com>
2020-04-26 02:56:05 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,6 @@
  def position_taken (board[" "," "," "," "," "," "," "," "," "], index)
-   if board[index] == 'X'
-
-   elsif board[index] == 'O'
+ end
+ 
 
 
 # code your #position_taken? method here!
